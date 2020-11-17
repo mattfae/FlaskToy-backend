3dd46de24bce58c77f723b452d04d5578aa2222a
msguttman <msguttman@gmail.com>
2020-11-16 19:33:15 -0600

    added linter

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Home</h1>'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
